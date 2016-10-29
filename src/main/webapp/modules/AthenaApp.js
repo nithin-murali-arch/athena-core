f223c1d597a9029d06f6dc0f3b9a71947f6a697c
Nithin Murali <nithin93@gmail.com>
2016-10-29 12:09:42 +0530

    adding factory

--- src/main/webapp/modules/AthenaApp.js
+++ src/main/webapp/modules/AthenaApp.js
@@ -15,24 +15,42 @@ app.config([ '$routeProvider', function($routeProvider) {
 app.factory('bnbObjHolder', function() {
 	var factory;
 	if (factory === undefined) {
-		factory = {};
+		factory = window.sessionStorage.getItem("factory");
+		if (factory === undefined) {
+			factory = {};
+		}
 	}
 
 	factory.setParam = function(key, value) {
 		factory[key] = value;
-	}
+	};
 
 	factory.getParam = function(key) {
 		return factory[key];
-	}
-
+	};
+	
+	factory.removeParam = function(key){
+		delete factory[key];
+	};
+	
+	factory.logout = function(){
+		removeParam('credentials');
+	};
+	
+	factory.persist = function(){
+		
+	};
 	return factory;
 });
 
-app.controller("LoginController", [ '$scope', function($scope) {
+app.controller("LoginController", [ '$scope', 'bnbObjectHolder', function($scope, bnbObjectHolder) {
+	$scope.logout = function(){
+		bnbObjectHolder.logout();
+	}
+	
+	$scope.test = "";
+}]);
 
-} ]);
+app.controller("JarUpload", [ '$scope', 'bnbObjectHolder', function($scope, bnbObjectHolder) {
 
-app.controller("JarUpload", [ '$scope', function($scope) {
-
-} ]);
+}]);
