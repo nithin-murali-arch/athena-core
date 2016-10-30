662509ec10c027b971379a63066b1fe71ce15f8c
Nithin Murali <Nithin Murali>
2016-10-30 21:26:05 +0530

    Stabilizing Build

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,24 +1,15 @@
 <html>
 <head>
-<link rel="stylesheet" href="css/bootstrap.min.css" />
 <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
 <link rel="stylesheet" href="css/athena-style.css" />
 <script type="text/javascript" src="js/angular.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.css"></script>
+<script type="text/javascript" src="js/angular-route.min.js"></script>
+<script type="text/javascript" src="css/bootstrap-min.css"></script>
 <script type="text/javascript" src="modules/AthenaApp.js"></script>
 </head>
 <body ng-app="AthenaApp">
 	<h2>Athena-Core</h2>
 	<div ng-view />
 </body>
-<script type="text/ng-template" id="addJar.htm">
-<form action="services/upload/jar" enctype="multipart/form-data" method="post">
-	<input type="file" name="file">
-	<input type="submit">
-</form>
-   </script>
-<script type="text/ng-template" id="login.htm">
-      <h2> Add Student </h2>
-      {{message}}
-   </script>
+
 </html>
--- src/main/webapp/modules/AthenaApp.js
+++ src/main/webapp/modules/AthenaApp.js
@@ -1,11 +1,11 @@
-var app = angular.module('athenaApp', [ 'ngRoute' ]);
+var app = angular.module('AthenaApp', [ 'ngRoute' ]);
 
 app.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/login', {
-		templateUrl : 'login.html',
+		templateUrl : 'modules/templates/login.html',
 		controller : 'LoginController'
 	}).when('/upload', {
-		templateUrl : 'addJar.html',
+		templateUrl : 'modules/templates/addJar.html',
 		controller : 'JarUpload'
 	}).otherwise({
 		redirectTo : '/login'
@@ -16,7 +16,7 @@ app.factory('bnbObjHolder', function() {
 	var factory;
 	if (factory === undefined) {
 		factory = window.sessionStorage.getItem("factory");
-		if (factory === undefined) {
+		if (factory === null) {
 			factory = {};
 		}
 	}
@@ -43,9 +43,9 @@ app.factory('bnbObjHolder', function() {
 	return factory;
 });
 
-app.controller("LoginController", [ '$scope', 'bnbObjectHolder', function($scope, bnbObjectHolder) {
+app.controller("LoginController", [ '$scope', 'bnbObjHolder', function($scope, bnbObjHolder) {
 	$scope.logout = function(){
-		bnbObjectHolder.logout();
+		bnbObjHolder.logout();
 	}
 	
 	$scope.test = "";
