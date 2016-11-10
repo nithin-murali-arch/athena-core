022755596ff808b7f489815a781313eaf6526679
Murali <nithin.murali@accenture.com>
2016-11-10 11:22:59 +0530

    Fixing jar upload bug

--- src/main/java/org/bnb/athena/queries/SQLQueries.java
+++ src/main/java/org/bnb/athena/queries/SQLQueries.java
@@ -3,7 +3,7 @@ package org.bnb.athena.queries;
 public interface SQLQueries {
 	String loginQuery = "SELECT USERNAME FROM ADMINS WHERE USERNAME = '?' AND PASSWORD = '#'";
 	String deleteQuery = "DELETE FROM JARS WHERE JARNAME = ?";
-	String insertQuery = "INSERT INTO JARS VALUES(?)";
+	String insertQuery = "INSERT INTO JARS VALUES('?')";
 	String listQuery = "SELECT * FROM JARS";
 	String addAppParam = "INSERT INTO APPPARAMS VALUES ('?','#')";
 	String listAppParam = "SELECT * FROM APPPARAMS";
